9b04e3277343f131caef8f451ff2fe230721f495
maritsandstad <marit.sandstad@astro.uio.no>
2021-10-04 13:49:51 +0200

    Setup and requirements files

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r") as fh:
+    requirements = [line.strip() for line in fh]
+
+setuptools.setup(
+    name="cicero-scm",
+    version="0.0.1",
+    author="Marit Sandstad",
+    author_email="marit.sandstad@cicero.oslo.no",
+    description="Python version of the cicero-scm simple climate model",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=requirements,
+)
