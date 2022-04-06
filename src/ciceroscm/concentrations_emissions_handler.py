b23f236f81ebc86465dacbe11f158b85c443626a
maritsandstad <marit.sandstad@astro.uio.no>
2022-04-06 16:23:17 +0200

    Updating docstrings for concentrations_emissions_handler.py

--- src/ciceroscm/concentrations_emissions_handler.py
+++ src/ciceroscm/concentrations_emissions_handler.py
@@ -20,6 +20,19 @@ LOGGER = logging.getLogger(__name__)
 def read_components(filename):
     """
     Read in components to be considered
+
+    Read the gas_pam files and rename a few headers to
+    make it easier to use
+
+    Parameters
+    ----------
+    filename : int
+            path to gaspamfile
+
+    Returns
+    -------
+    pandas.Dataframe
+        Dataframe with gases, and various info on them
     """
     df_gas = pd.read_csv(filename, delim_whitespace=True, index_col=0)
     df_gas.rename(
@@ -31,7 +44,21 @@ def read_components(filename):
 def _rs_function(it, idtm=24):
     """
     Calculate pulse response function for mixed layer
+
+    Calculate pulse response function for mixed layer
     time is the year index*idtm + i, i.e. the month number
+
+    Parameters
+    ----------
+    it : int
+      is the time index, there are idtm time points per yer
+    idtm : int
+        Number of time points per year, default is 24
+
+    Returns
+    -------
+    float
+         The pulse_response function for this time
     """
     time = it / idtm
     if time < 2.0:
@@ -57,7 +84,21 @@ def _rs_function(it, idtm=24):
 def _rb_function(it, idtm=24):
     """
     Calculate biotic decay function
+
+    Calculate biotic decay function
     time is the year index*idtm + i, i.e. the month number
+
+    Parameters
+    ----------
+    it : int
+      is the time index, there are idtm time points per yer
+    idtm : int
+        Number of time points per year, default is 24
+
+    Returns
+    -------
+    float
+        The biotic decay function value for this time
     """
     time = it / idtm
     biotic_decay = (
@@ -82,7 +123,20 @@ def read_natural_emissions(filename, component, startyear=1750, endyear=2500):
 def check_pamset(pamset):
     """
     Check that parameterset has necessary values for run
-    Otherwise set to default values
+
+    Check that parameterset has necessary values for run
+    Otherwise set to default values which are defined here
+
+    Parameters
+    ----------
+    pamset : dict
+          Dictionary of parameters to define the physics
+          of the run
+
+    Returns
+    -------
+    dict
+        Updated pamset with default values used where necessary
     """
     required = {
         "lamb": 0.8,
@@ -106,6 +160,22 @@ def check_pamset(pamset):
 def check_pamset_consistency(pamset_old, pamset_new):
     """
     Make sure new pamset is consistent with existing instance
+
+    This method is meant to ensure consistency when rerunning
+    with the same class instance. Changing to old values
+    if they are different, or setting to old values if none are
+    given
+
+    Parameters
+    ----------
+    pamset_old : dict
+              Original parameterset for instance
+    pamset_new : dict
+              New pamset for new run
+    Returns
+    -------
+    dict
+        The new pamset, augmented to fit the old one
     """
     pams_cant_change = ["idtm", "nystart", "nyend", "emstart", "conc_run"]
     for pam in pams_cant_change:
@@ -123,6 +193,24 @@ def check_pamset_consistency(pamset_old, pamset_new):
 def read_inputfile(input_file, cut_years=False, year_start=1750, year_end=2100):
     """
     Read input from emissions or concentrations file
+
+    Parameters
+    ----------
+    input_file : str
+              Path to file to be read in
+    cut_years : bool
+             If unused years are to be cut, this option should
+             be set to True, default is False
+    year_start : int
+             Start year for relevant input, default is 1750
+    year_end : int
+             End year for relevant input, default is 2100
+
+    Returns
+    -------
+    pandas.Dataframe
+        Dataframe with the intput from the file, possibly cut
+        to relevant years
     """
     df_input = pd.read_csv(
         input_file, delim_whitespace=True, index_col=0, skiprows=[1, 2, 3]
@@ -149,6 +237,28 @@ class ConcentrationsEmissionsHandler:
     def __init__(
         self, cfg, pamset,
     ):
+        """
+        Intialising concentrations emissions handler
+
+        Starting by reading in gasparameter file, making
+        empty dicts for concentrations and forcing,
+        reading in natural emissions, checking the pamset,
+        and making an array of years to look at.
+        Then reading in concentrations and emissions,
+        perturbations if relevant.
+        Then initialising pulse and biotic functions,
+        finally intialising empty arrays and setting
+        CO2 start values.
+
+        Parameters
+        ----------
+        cfg : dict
+           configurations such as input file locations
+           Whether run is concentration run, if perturbations
+           should be included etc.
+        pamset : dict
+           list of physical parameters to define the run
+        """
         self.df_gas = read_components(cfg["gaspamfile"])
         self.conc = {}
         self.forc = {}
@@ -201,6 +311,19 @@ class ConcentrationsEmissionsHandler:
     def reset_with_new_pams(self, pamset, preexisting=True):
         """
         Reset to run again with same emissions etc.
+
+        Resetting arrays and co2 start values also
+        making sure parameterset conforms with previously
+        existing set if new run is being run
+
+        Parameters
+        ----------
+        pamset : dict
+              Dictionary of physical parameters
+        preexisting : bool
+                   Defining whether this is the first use of the class
+                   or a new run with the same instance, that has to
+                   conform to old values
         """
         if preexisting:
             new_pamset = check_pamset(pamset)
@@ -224,8 +347,21 @@ class ConcentrationsEmissionsHandler:
     def calculate_strat_quantities(self, yr):
         """
         Calculate sumcl and sumbr in stratosphere
-        sums of effects on stratospheric ozone from
-        chlorinated gases and halons
+
+        Calculating sum of stratospheric effects of
+        chlorinated gase and sum of stratospheric effects
+        of halon quantities.
+
+        Parameters
+        ----------
+        yr : int
+          Year for which to calculate
+
+        Returns
+        -------
+        list
+            Containing the sumcl, value for chlorinated gases
+            and the sumbr value for halon gases.
         """
         if yr < self.years[0]:
             return 0.0, 0.0
@@ -255,6 +391,21 @@ class ConcentrationsEmissionsHandler:
     def calculate_forc_three_main(self, yr):  # pylint: disable=too-many-locals
         """
         Calculate forcings for components CO2, N2O and CH4
+
+        Method to calculate the forcing from the main three componenents
+        as these three are a bit intertwined. Using a coefficient matrix
+
+        Parameters
+        ----------
+        yr : int
+          Year for which to calculate
+
+        Returns
+        -------
+        list
+            Containing the total forcing from the three, and the
+            combinded forcing value on each hemisphere
+            tot_forc, forc_nh, forc_sh
         """
         # Setting some constants. What are they? Should they be parametrisable?
         # (Etminan I guess...)
@@ -319,6 +470,23 @@ class ConcentrationsEmissionsHandler:
     def tropospheric_ozone_forcing(self, yr):
         """
         Calculate tropospheric ozone forcing
+
+        Using the emissions of various other gases
+        Before emissions start, the fossil fuel CO2
+        change from the start is used
+        After emissions start, concentrations of
+        methane are combined with emissions of NOx, CO and NMVOC
+        to calculate a total troposperic ozone forcing
+
+        Parameters
+        ----------
+        yr : int
+          Year for which to calculate
+
+        Returns
+        -------
+        float
+             tropospheric ozone forcing
         """
         emstart = self.pamset["emstart"]
         yr_0 = self.years[0]
@@ -362,6 +530,35 @@ class ConcentrationsEmissionsHandler:
     def conc2forc(self, yr, rf_luc, rf_sun):
         """
         Calculate forcing from concentrations
+
+        Looping through all species and getting the
+        forcing from their concentrations. First calling
+        a method to calculate intial forcing for three main
+        species, CO2, N2O and CH4. This will also initialise
+        total forcing and hemispherically split forcing.
+        Then looping over species
+        calculated dirctly from emissions (SO2, SO4_IND, OC and BC)
+        Then looping over other gases with alpha factor,
+        then ozone in troposphere and stratosphere, and stratospheric water
+        vapour, finally other species, and adding perturbations if
+        present. For every species add to total forcing and hemispherically
+        split forcings, finally add solar forcing.
+
+        Parameters
+        ----------
+        yr : int
+          Year fro which to calculate
+        rf_luc : float
+              Land use change forcing
+        rf_sun : float
+              Solar forcing
+
+        Returns
+        -------
+        list
+            Consisting of total forcing and hemispherically split
+            forcing for the year.
+            In this way: tot_forc, forc_nh, forc_sh
         """
         ref_emission_species = {
             "SO2": ["SO2", self.pamset["qdirso2"]],
@@ -441,6 +638,17 @@ class ConcentrationsEmissionsHandler:
     def emi2conc(self, yr):
         """
         Calculate concentrations from emissions
+
+        If conc_run or prior to emissions start, concentrations
+        are simply read in from existing data frame, after that
+        all tracers are looped over to calculate concentrations
+        from emissions. For CO2 this is doen in a separate method.
+        For CH4 and N2O, natural emissions are added.
+
+        Parameters
+        ----------
+        yr : int
+          Year for which to calculate
         """
         # Do per tracer emissions to concentrations, update concentrations df
         # NBNB! Remember to move calculation of  Trop_O3 concentration
@@ -499,6 +707,22 @@ class ConcentrationsEmissionsHandler:
     def methane_lifetime(self, q, conc_local, yr):
         """
         Calculate methane concentrations from emissions
+
+        Calculate methane concentrations from emissions
+        using different liftetime modes to calculate
+
+        Parameters
+        ----------
+        q : float
+         Forcing without feedbacks
+        conc_local : float
+                  Concentration of methane in previous timestep
+        yr : int
+          Year for which to calculate
+        Returns
+        -------
+        float
+             Concentrations adjusted for lifetime / feedback
         """
         ch4_wigley_exp = -0.238
         if self.pamset["lifetime_mode"] == "TAR":
@@ -522,6 +746,15 @@ class ConcentrationsEmissionsHandler:
     def co2em2conc(self, yr):  # pylint: disable=too-many-locals
         """
         Calculate co2 concentrations from emissions
+
+        Method to calculate co2 concentrations from emissions
+        Implementing a rudimentary carbon cycle which loops over
+        idtm (usually 24) timesteps a year
+
+        Parameters
+        ----------
+        yr : int
+          Year for which to calculate
         """
         # Fertilisation factor
         beta_f = 0.287
@@ -630,6 +863,14 @@ class ConcentrationsEmissionsHandler:
     def add_row_of_zeros_conc(self, yr):
         """
         Fill in one row of concentrations in conc_dict
+
+        Fill in a row of zeros in concentrations dictionary
+        for a single year
+
+        Parameters
+        ----------
+        yr : int
+          Year for which to add zeros
         """
         for value_dict in self.conc.values():
             value_dict[yr] = 0
@@ -637,6 +878,18 @@ class ConcentrationsEmissionsHandler:
     def write_output_to_files(self, cfg):
         """
         Write results to files after run
+
+        Write results for emissions, concentrations
+        and forcings to files. Format is as in the
+        fortran implementation, with separate files for
+        emissions (emis), concentrations (conc) and
+        forcing (forc)
+
+        Parameters
+        ----------
+        cfg : dict
+           Configurations to define where to put output
+           files and what prefix to have for file name
         """
         if "output_folder" in cfg:
             # Make os independent?
