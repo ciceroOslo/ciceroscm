d3024e4bce924ac5e8936b2fabd50a3ea181acd5
maritsandstad <marit.sandstad@astro.uio.no>
2023-10-11 11:25:51 +0200

    Clearing out unused density methods

--- tests/unit/test_upwellingDownwellingModel.py
+++ tests/unit/test_upwellingDownwellingModel.py
@@ -3,25 +3,6 @@ import numpy as np
 from ciceroscm import upwelling_diffusion_model
 
 
-def test_coefic():
-    assert upwelling_diffusion_model._coefic(0, 0, 0) == 19652.21
-    assert upwelling_diffusion_model._coefic(1, 0, 0) == 19706.96404
-    assert upwelling_diffusion_model._coefic(0, 1, 0) == 19798.31704821712
-    assert upwelling_diffusion_model._coefic(0, 0, 1) == 19655.449993093498
-
-
-def test_denso():
-    assert upwelling_diffusion_model._denso(0, 0) == 999.842594
-    assert upwelling_diffusion_model._denso(0, 1) == 999.9015372849533
-    assert upwelling_diffusion_model._denso(1, 0) == 1000.6618454799999
-
-
-def test_density():
-    assert upwelling_diffusion_model._density(0, 0) == 1028.1063314148107
-    assert upwelling_diffusion_model._density(1, 0) == 1028.1539628244798
-    assert upwelling_diffusion_model._density(0, 1) == 1028.0456085605597
-
-
 # Rewrite test for class method
 def test_band():
     udm = upwelling_diffusion_model.UpwellingDiffusionModel(
