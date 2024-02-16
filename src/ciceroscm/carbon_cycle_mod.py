Fixing to meaningful calculations with right units for ocean and lan carbon sinks

src/ciceroscm/carbon_cycle_mod.py
@@ -441,11 +441,14 @@ class CarbonCycleModel:
             Timeseries of the added carbon content to the biosphere carbon pool
         """
         ffer = self._get_ffer_timeseries(conc_run, co2_conc_series)
-        biosphere_carbon_pool = np.array(
-            [
-                np.sum(ffer[: self.pamset["idtm"] * (yrix + 1)])
-                for yrix in range(self.pamset["years_tot"])
-            ]
+        biosphere_carbon_pool = (
+            np.array(
+                [
+                    np.sum(ffer[: self.pamset["idtm"] * (yrix + 1)])
+                    for yrix in range(self.pamset["years_tot"])
+                ]
+            )
+            / self.pamset["idtm"]
         )
         return biosphere_carbon_pool
 
@@ -465,11 +468,17 @@ class CarbonCycleModel:
         #    np.sum(self.co2_hold["sCO2"][: self.pamset["idtm"] * (yrix+1)])
         #    for yrix in range(self.pamset["years_tot"])
         # ])
-        ocean_carbon_pool = np.array(
-            [
-                self.co2_hold["sCO2"][self.pamset["idtm"] * (yrix + 1) - 1]
-                for yrix in range(self.pamset["years_tot"])
-            ]
+        ocean_carbon_pool = (
+            np.array(
+                [
+                    np.sum(self.co2_hold["sCO2"][: self.pamset["idtm"] * (yrix + 1)])
+                    for yrix in range(self.pamset["years_tot"])
+                ]
+            )
+            * PPM_CO2_TO_PG_C
+            / self.pamset["idtm"]
+            * GE_COEFF
+            * OCEAN_AREA
         )
         return ocean_carbon_pool
 

tests/integration/test_scm_conc.py
@@ -195,12 +195,8 @@ def test_ciceroscm_short_run(tmpdir, test_data_dir):
     ]
     for key in expected_keys:
         assert key in cscm.results
-    print(cscm.results.keys())
     for key in cscm.results.keys():
         assert key in expected_keys
-    print(cscm.results["carbon cycle"]["Airborne fraction CO2"])
-    print(cscm.results["carbon cycle"]["Biosphere carbon pool"])
-    print(cscm.results["carbon cycle"]["Ocean carbon pool"])
     assert len(cscm.results["carbon cycle"]["Airborne fraction CO2"]) == len(
         cscm.ce_handler.years
     )
@@ -209,6 +205,20 @@ def test_ciceroscm_short_run(tmpdir, test_data_dir):
     )
     assert len(cscm.results["carbon cycle"]["Ocean carbon pool"]) == len(
         cscm.ce_handler.years
+    )
+    carbon_sum = (
+        np.cumsum(
+            cscm.results["carbon cycle"]["Airborne fraction CO2"]
+            * cscm.results["carbon cycle"]["Emissions"]
+        )
+        + cscm.results["carbon cycle"]["Biosphere carbon pool"].values
+        + cscm.results["carbon cycle"]["Ocean carbon pool"].values
+    )
+    print(carbon_sum[-5:])
+    print(np.cumsum(cscm.results["carbon cycle"]["Emissions"])[-5:])
+    # TODO: Why isn't this closer?
+    assert np.allclose(
+        carbon_sum, np.cumsum(cscm.results["carbon cycle"]["Emissions"]), rtol=5e-1
     )
     # Put this in again, find out what is happening with CF4
     # check_output(