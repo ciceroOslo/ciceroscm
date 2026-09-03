fe858e640838cae52aafb22b1988833074540960
Luke Western <lwestern@mit.edu>
2026-09-03 16:40:02 -0400

    Make neater by including EESC weights and FRFs in constants

--- src/ciceroscm/constants.py
+++ src/ciceroscm/constants.py
@@ -35,3 +35,58 @@ AEROSOL_TRACERS = (
     "NH3",
     "BMB_AEROS",
 )
+
+# Fractional release factors used for equivalent effective stratospheric
+# chlorine (EESC) calculations, per halocarbon species.
+EESC_FRF = {
+    "CFC-11": 0.47,
+    "CFC-12": 0.24,
+    "CFC-13": 0.06,
+    "CFC-112": 0.3,
+    "CFC-112a": 0.53,
+    "CFC-113": 0.30,
+    "CFC-113a": 0.29,
+    "CFC-114": 0.13,
+    "CFC-114a": 0.32,
+    "CFC-115": 0.07,
+    "CCl4": 0.56,
+    "CH3CCl3": 0.61,
+    "HCFC-133a": 0.4,
+    "HCFC-22": 0.15,
+    "HCFC-141b": 0.34,
+    "HCFC-142b": 0.17,
+    "HCFC-123": 0.66,
+    "HCFC-124": 0.32,
+    "HCFC-31": 0.67,
+    "H-1211": 0.65,
+    "H-1202": 0.67,
+    "H-1301": 0.32,
+    "H-2402": 0.66,
+    "CH3Br": 0.6,
+    "CH3Cl": 0.44,
+}
+
+# Age-of-air weighting used to smear yearly EESC source terms over the
+# preceding 20 years (age-spectrum convolution kernel).
+EESC_WEIGHTS = [
+    2.51540874e-01,
+    2.85588337e-01,
+    1.83647911e-01,
+    1.09745490e-01,
+    6.53733900e-02,
+    3.93816364e-02,
+    2.40508141e-02,
+    1.48813664e-02,
+    9.31632867e-03,
+    5.89277991e-03,
+    3.76106432e-03,
+    2.41954343e-03,
+    1.56739065e-03,
+    1.02163109e-03,
+    6.69558679e-04,
+    4.40970373e-04,
+    2.91703198e-04,
+    1.93730846e-04,
+    1.29128440e-04,
+    8.63517467e-05,
+]
